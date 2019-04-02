10f4a411eaf342c1082c0c90acea9848f505500d
shunyk <48780737+shunyk@users.noreply.github.com>
2019-04-02 15:17:52 +0900

    add

--- src/com/shunyk/lang/wrapper/WrapperMain.java
+++ src/com/shunyk/lang/wrapper/WrapperMain.java
@@ -5,6 +5,7 @@ public class WrapperMain {
 	public static void main(String[] args) {
 		Wrapper_ex1 we1 = new Wrapper_ex1();
 		Wrapper_ex2 we2 = new Wrapper_ex2();
+		Wrapper_ex3 we3 = new Wrapper_ex3();
 		
 		/*we1.ex1();
 		
@@ -13,10 +14,27 @@ public class WrapperMain {
 		System.out.println(Integer.toOctalString(num));
 		System.out.println(Integer.toHexString(num));
 
-		we1.ex2();*/
+		we1.ex2();
 		
-		we2.ex1();
+		//we2.ex1();
 		
+		Integer n1 = new Integer(10);
+		int num = n1;
+		
+		double d1 = 3.14;
+		d1 = Math.ceil(d1); //올림
+		System.out.println(d1);
+		d1 = 3.64;
+		d1 = Math.floor(d1); //내림
+		System.out.println(d1);
+		d1 = 3.45;
+		d1 = Math.round(d1); //반올림
+		System.out.println(d1);
+		
+		d1 = Math.random();
+		System.out.println("Random : " + d1);*/
+		
+		we3.ex1();
 	}
 
 }
---
+++ src/com/shunyk/lang/wrapper/Wrapper_ex3.java
@@ -0,0 +1,10 @@
+package com.shunyk.lang.wrapper;
+
+public class Wrapper_ex3 {
+	
+	public void ex1() {
+		int num = (int) (Math.random()*9)+1;
+		System.out.println(num);
+	}
+	
+}
